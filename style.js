8c3c244814dfb2af976f6416c3a7a3d9f3a25bd8
Uzun Igor <te10@mail.ru>
2020-03-02 14:45:07 +0200

    made hw 10 JS

---
+++ style.js
@@ -0,0 +1,23 @@
+let myCurrentAge = 31;
+if (myCurrentAge >= 25) {
+   console.log ('I am over 25 years old'); 
+} else {
+    console.log ('I am under 25') 
+};
+
+console.log((myCurrentAge >= 25) ? "I am over 25 years old" : "I am under 25");
+
+let a = 12;
+console.log(typeof a);
+let b = 'lorem';
+console.log(typeof b);
+let c = false;
+console.log(typeof c);
+let d;
+console.log(typeof d);
+let e = {name: "Igor"};
+console.log(typeof e);
+let f = function(){};
+console.log(typeof f);
+let g = null;
+console.log(typeof g); //bag object == null
